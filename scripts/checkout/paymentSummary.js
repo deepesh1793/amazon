bf19cad5e9471dcc29bb06139d7def8c26381bae
Deepesh <deepesh1793@gmail.com>
2025-08-13 21:27:21 +0530

    place order feature added

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,5 +1,6 @@
 import { cart } from "../../data/cart.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
+import { addOrder } from "../../data/orders.js";
 import { getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 
@@ -51,10 +52,32 @@ export function renderPaymentSummary() {
       <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order">
       Place your order
     </button>
   `;
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order').addEventListener('click', async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          cart: cart
+        }),
+      });
+
+      const order = await response.json();
+      addOrder(order);
+    }
+    catch (error) {
+      console.error('Error placing order:', error);
+    }
+
+    window.location.href = 'orders.html';
+  });
 }
